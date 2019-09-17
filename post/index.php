e049e4fd5a81fe8f484b444ae666e4a830c50a12
namida-neet <namida.neet@gmail.com>
2019-09-17 22:26:53 +0900

    chapter6-8 P274

--- post/index.php
+++ post/index.php
@@ -31,6 +31,15 @@ if (!empty($_POST)) {
 
 // 投稿を取得する
 $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+
+// 返信の場合
+if (isset($_REQUEST['res'])) {
+    $response = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=? ORDER BY p.created DESC');
+    $response->execute(array($_REQUEST['res']));
+
+    $table = $response->fetch();
+    $message = '@' . $table['name'] . ' ' . $table['message'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -56,7 +65,8 @@ $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE
         <dl>
           <dt><?php echo htmlspecialchars($member['name'], ENT_QUOTES); ?>さん、メッセージをどうぞ</dt>
           <dd>
-            <textarea name="message" id="" cols="50" rows="5"></textarea>
+            <textarea name="message" id="" cols="50" rows="5"><?php if (isset($message)) { echo htmlspecialchars($message, ENT_QUOTES);} ?></textarea>
+            <input type="hidden" name="reply_post_id" value="<?php if(isset($_REQUEST['res'])) { echo htmlspecialchars($_REQUEST['res'], ENT_QUOTES); } ?>">
           </dd>
         </dl>
         <div>
@@ -67,7 +77,10 @@ $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE
       <?php foreach ($posts as $post) : ?>
       <div class="msg">
         <img src="member_pictures/<?php echo htmlspecialchars($post['picture'], ENT_QUOTES); ?>" width="48" height="48" alt="<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>">
-        <p><?php echo htmlspecialchars($post['message'], ENT_QUOTES); ?><span class="name">(<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>)</span></p>
+        <p>
+          <?php echo htmlspecialchars($post['message'], ENT_QUOTES); ?><span class="name">(<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>)</span>
+          [<a href="index.php?res=<?php echo htmlspecialchars($post['id'], ENT_QUOTES); ?>">Re</a>]
+        </p>
         <p class="day"><?php echo htmlspecialchars($post['created'], ENT_QUOTES); ?></p>
       </div>
       <?php endforeach; ?>
